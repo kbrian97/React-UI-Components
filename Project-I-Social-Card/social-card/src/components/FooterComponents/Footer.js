83cc2080ec13e6c6b8678adfee9104640aba1245
kbrian97 <ktorgbede2015@fau.edu>
2019-02-27 23:17:28 -0500

    footer counter next heart background

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import './Footer.css';
+import {Component} from 'react';
 
+class App extends Component {
+    constructor(props) {
+    super(props);
+        this.state = {
+            clicks1: 0,
+            clicks2: 0,
+            clicks3: 0,
+            clicks4: 0,
+            show: true
+        };
+    }
 
-function Footer() {
+    IncrementItem = () => {
+    this.setState({ clicks1: this.state.clicks1 + 1 });
+    }
+
+    IncrementItem2 = () => {
+        this.setState({ clicks2: this.state.clicks2 + 1 });
+    }
+    
+    IncrementItem3 = () => {
+        this.setState({ clicks3: this.state.clicks3 + 1 });
+    }
+    
+    IncrementItem4 = () => {
+        this.setState({ clicks4: this.state.clicks4 + 1 });
+    }
+    
+
+    render() {
     return(
         <div className='Footer'>
-            <button><img src='https://image.flaticon.com/icons/svg/1246/1246332.svg' className='icon 1'></img></button>
-            <button><img src='https://image.flaticon.com/icons/svg/1246/1246245.svg' className='icon 2'></img></button>
-            <button><img src='https://image.flaticon.com/icons/svg/149/149217.svg' className='icon 3'></img></button>
-            <button><img src='https://image.flaticon.com/icons/svg/784/784677.svg' className='icon 4'></img></button>
+            <button onClick={this.IncrementItem}><img src='https://image.flaticon.com/icons/svg/1246/1246332.svg' className='icon A'></img>{this.state.clicks1}</button>
+            <button onClick={this.IncrementItem2}><img src='https://image.flaticon.com/icons/svg/1246/1246245.svg' className='icon B'></img>{this.state.clicks2}</button>
+            <button onClick={this.IncrementItem3}><img src='https://image.flaticon.com/icons/svg/149/149217.svg' className='icon C'></img>{this.state.clicks3}</button>
+            <button onClick={this.IncrementItem4}><img src='https://image.flaticon.com/icons/svg/784/784677.svg' className='icon D'></img>{this.state.clicks4}</button>
         </div>
         );
-}
+        }
+    }
 
-export default Footer;
+    export default App;
